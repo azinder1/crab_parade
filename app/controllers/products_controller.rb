ba9e2155dde66aec3912996afebdf2bc87d13c40
Adam Zinder <adam.zinder@gmail.com>
2017-02-17 16:13:51 -0800

    block users from accessing pages unless they are given admin priviledges

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,16 +3,21 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
   def show
     @product = Product.find(params[:id])
     @user = current_user
     @review = Review.new
   end
+
   def new
+    if !current_user.admin
+      redirect_to root_path
+    end
     @product = Product.new
   end
-  def create
 
+  def create
     @product = Product.create(product_params)
     if @product.save
       redirect_to products_path
@@ -22,6 +27,9 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    if !current_user.admin
+      redirect_to root_path
+    end
     @product = Product.find(params[:id])
   end
 
@@ -34,11 +42,13 @@ class ProductsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
+
   private
 
   def product_params
